1dd62973b61355f7dbe031f72b51dafbe9b26e07
Thibault Roux <Thibault.Roux1@laposte.net>
2024-07-25 23:35:11 +0200

    Use normal law instead

--- code.py
+++ code.py
@@ -2,10 +2,16 @@ import numpy as np
 import sklearn
 
 if __name__ == "__main__":
+    number = 100
     # generate random size in centimeters for women
-    women_sizes = np.random.randint(140, 160, 100)
+    women_sizes = np.random.normal(150, 10, number)
+    # generate random weight in kilograms for women
+    women_weights = np.random.normal(50, 10, number)
     # generate random size in centimeters for men
-    men_sizes = np.random.randint(170, 200, 100)
+    men_sizes = np.random.normal(180, 10, number)
+    # generate random weight in kilograms for men
+    men_weights = np.random.normal(80, 10, number)
+
 
     # create a shuffle dataset with the sizes of both women and men
     dataset = []
@@ -14,3 +20,11 @@ if __name__ == "__main__":
     for size in men_sizes:
         dataset.append((size, 1))
     print(dataset)
+
+    # shuffle the dataset and split in training in testing set
+    np.random.shuffle(dataset)
+    train_set = dataset[:number]
+    test_set = dataset[number:]
+
+    # create the model
+
