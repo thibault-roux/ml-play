00a5267734c64e441a15297d20932a7d2bf798ec
Thibault Roux <Thibault.Roux1@laposte.net>
2024-07-26 00:21:06 +0200

    Plot decision boundary and choose number of epoch

--- mycode.py
+++ mycode.py
@@ -6,13 +6,13 @@ from sklearn.inspection import DecisionBoundaryDisplay
 if __name__ == "__main__":
     number = 10000
     # generate random size in centimeters for women
-    women_sizes = np.random.normal(130, 10, number)
+    women_sizes = np.random.normal(160, 10, number)
     # generate random weight in kilograms for women
-    women_weights = np.random.normal(40, 10, number)
+    women_weights = np.random.normal(60, 10, number)
     # generate random size in centimeters for men
-    men_sizes = np.random.normal(190, 10, number)
+    men_sizes = np.random.normal(180, 10, number)
     # generate random weight in kilograms for men
-    men_weights = np.random.normal(90, 10, number)
+    men_weights = np.random.normal(80, 10, number)
 
 
     # create a shuffle dataset with the sizes and weight of both women and men
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     model = Perceptron()
     # train the model
     print("Training...")
-    model.fit([x[:2] for x in train_set], [x[2] for x in train_set])
+    model.fit([x[:2] for x in train_set], [x[2] for x in train_set], epochs=5)
     # test the model
     print("Testing...")
     print(model.score([x[:2] for x in test_set], [x[2] for x in test_set]))
@@ -47,6 +47,12 @@ if __name__ == "__main__":
 
     # plot the decision boundary
     display = DecisionBoundaryDisplay.from_estimator(model, X=np.array([x[:2] for x in dataset]), xlabel="Size", ylabel="Weight")
+
+    # plot a small subset of the dataset
+    dataset = dataset[:100]
+    plt.scatter([x[0] for x in dataset if x[2] == 0], [x[1] for x in dataset if x[2] == 0], color="red", label="women")
+    plt.scatter([x[0] for x in dataset if x[2] == 1], [x[1] for x in dataset if x[2] == 1], color="blue", label="men")
+
     plt.plot()
     plt.savefig("decision_boundary.png")
 
